3213659823160be2ecdca45af30ede3f2421742d
unknown <alkhaibari33@gmail.com>
2019-02-20 18:10:04 +0300

    ADEL

--- dom/js/main.js
+++ dom/js/main.js
@@ -1,82 +1,25 @@
-// console.log(document.getElementById("myid"))
-// console.log(document.getElementsByClassName("hot")[0])
-// console.log(document.querySelectorAll(".hot"))
-// console.log(document.querySelector("#myid"))
+var hourHand= document.getElementById('hourHand'):
+var minuteHand = document .getElementById('minuteHand');
+var secondHand = document .getElementById('secondHand');
 
-//selects all classes with class name hot
-var hot = document.getElementsByClassName("hot")
+funcation initClock(){
+    var date=new Date();
+    var hour =date.getHours() % 12;
+    var minute = date.getMinutes();
+    var secondHand= date. getSeconds();
 
-hot[2].style.color = "red" //changes text color
-hot[0].setAttribute("data-id", "blue") //set attribute to element
-//<li class="hot" data-id="blue">two</li>
-hot[0].className = "blue"//<li class="blue">two</li>
+    var hourDeg = (hour * 30) + (0.5 * minute);
+    var minuteDeg = (minute * 6) +(0.1 * second);
+    var secondDeg = second * 6;
 
-var list = document.getElementById("list") //gets single element
-var item = document.createElement("li") //creates element
-//<li></li>
-item.setAttribute("class", "hot")
-//<li class="hot"></li>
-// var text = document.createTextNode("five")
-// five
-item.appendChild(document.createTextNode("five"))
-//<li class="hot">five</li> 
-list.appendChild(item)
-//
-function add(){
-    var input = document.querySelector("#info").value
-    
-    if(input === ""){
-    // console.log(input)
-    let items = document.createElement("li")
-    items.appendChild(document.createTextNode(input))
-
-    list.appendChild(items)
-
-    document.querySelector("#info").value = ""
-    }
-}
+    hourHand. style.transform = 'rotate('+ hourDeg + 'deg)';
+    minuteHand. style.transform = 'rotate('+ hourDeg + 'deg)';
+    secondHand. style.transform = 'rotate('+ hourDeg + 'deg)';
 
 
 
-document.querySelector("#btn").addEventListener("click",createTable)
-// document.querySelector("#btn").onclick = createTable
-//Read about parentNodes, SiblingNodes, childnodes
-//.parentNode 
-//e.target
 
-//create table
-function createTable(e){
-    e.preventDefault()
-    e.target
-   //select table container
-    var tbl = document.querySelector(".tbl")
+    setTimeout( initClock, 1000);
 
-    var row_val = document.querySelector("#row").value
-    var col_val = document.querySelector("#col").value
-
-    //create table element
-    var table = document.createElement("table")
-    var tbody = document.createElement("tbody")
-    
-    
-
-    for(var i = 0; i < row_val; i++){
-
-        var tr = document.createElement("tr")
-        for(var j = 0; j < col_val; j++){
-
-            var td = document.createElement("td")
-            td.setAttribute("onclick", "style.color ='red'")
-            var text = document.createTextNode(i + ", " + j)
-                                    //`${i}, ${j}`
-            td.appendChild(text)
-
-            tr.appendChild(td)
-            
-        }
-        tbody.appendChild(tr)
-    }
-    table.appendChild(tbody)
-    tbl.appendChild(table)
-
-}
+};
+initClock();
